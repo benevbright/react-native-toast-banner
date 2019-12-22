94cd2b4bbe37c9efc446fbe9ffa3fdb70903f56f
Bright Lee <benevbright@gmail.com>
2019-12-22 11:21:41 +0100

    example: setup basic react-navigation

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import SafeAreaView from 'react-native-safe-area-view';
-import {A} from 'react-native-toast-banner';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-const App = () => (
-  <SafeAreaProvider>
-    <SafeAreaView>
-      <View>
-        <A />
-        <Text>Hello</Text>
-      </View>
-    </SafeAreaView>
-  </SafeAreaProvider>
+const HomeScreen = () => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text>Home Screen</Text>
+  </View>
 );
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      title: 'react-native-toast-banner',
+    },
+  },
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+const App = () => <AppContainer />;
 
 export default App;
